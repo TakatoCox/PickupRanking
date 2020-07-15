1bc9bb8796d2f24ea6bf8ce21a64443a0316f624
Takato Cox <takatohockey@gmail.com>
2020-07-15 11:50:37 -0700

    bug

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //connect mongoDB
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(()=>console.log("Database connected!"))
 .catch(err=>{console.log(`DB Connection Error: ${err.message}`)});
 
